7db2d32166505060bd57c4c7a537fece0444be14
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-01-13 16:03:52 +0100

    first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,13 @@
+import * as z from "zod"
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, { message: 'Name must be at least 2 characters' }),
+    username: z.string().min(6, {message: 'Username must be at least 6 characters' }).max(50),
+    email: z.string().email(),
+    password: z.string().min(8, { message: 'Password must be at least 8 characters' })
+})
+
+export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, { message: 'Password must be at least 8 characters' })
+})
