5d2244e4fe31b6d134bf2f69258ace9ef917ba17
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-01-13 21:54:54 +0100

    Mobile responsive

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import TopBar from '@/components/ui/shared/TopBar'
+import LeftSidebar from '@/components/ui/shared/LeftSidebar'
+import { Outlet } from 'react-router-dom'
+import Bottombar from '@/components/ui/shared/Bottombar'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className='w-full md:flex '>
+      <TopBar />
+      <LeftSidebar />
+
+      <section className='flex flex-1 h-full'>
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
   )
 }
 
