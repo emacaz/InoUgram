61a9df02ce1bc0d705d53d54ebe638e8f7f0699e
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-01-14 17:16:55 +0100

    Debugging at uploading pics

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -5,8 +5,12 @@ import {
   useInfiniteQuery,
 } from "@tanstack/react-query";
 
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import {
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
+import { INewPost, INewUser } from "@/types";
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -23,6 +27,18 @@ export const useSignInAccount = () => {
 
 export const useSignOutAccount = () => {
   return useMutation({
-    mutationFn: signOutAccount
+    mutationFn: signOutAccount,
   });
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  // // return useMutation({
+  // //   mutationFn: (post: INewPost) => createPost(post),
+  // //   onSuccess: () => {
+  // //     queryClient.invalidateQueries({
+  // //       queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+  // //     });
+  // //   },
+  // });
 };
